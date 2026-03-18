40b3b5be36cabf56e9bfaeb47704315f471e8993
Eric Cheyssial <echeyssial@csus.edu>
2026-03-18 12:40:10 -0700

    Add functions to increase font size and style text.

--- assn5-fancify/fancifymytext.js
+++ assn5-fancify/fancifymytext.js
@@ -1,16 +1,40 @@
+// Eric Cheyssial | CSC 193A | Assignment 5
 
+// Main buttons.
 const biggerButton = document.querySelector("#biggerButton");
+const fancyButton = document.querySelector("#fancyShmancy");
+const boringButton = document.querySelector("#boringBetty");
 
-function alertBiggerClicked() {    
-    alert("Hello, world!");
-}
+// HTML textarea reference.
+const textArea = document.getElementById("userText");
 
-biggerButton.onclick = alertBiggerClicked;
+// Original alert for "Bigger!" click.
+/*  function alertBiggerClicked() {    
+        alert("Hello, world!");
+    } */
 
+// biggerButton.onclick = alertBiggerClicked;
 
+function increaseFontSize() {
+  textArea.style.fontSize = "24pt";
+} // function increaseFontSize
 
+function styleText() {
+    if (fancyButton.checked) {
+        textArea.style.fontWeight = "bold";
+        textArea.style.color = "blue";
+        textArea.style.textDecoration = "underline";
 
+    // Assess whether "BoringBetty" was checked, revert font.
+    } else if (boringButton.checked) {
+        textArea.style.fontWeight = "normal";
+        textArea.style.color = "black";
+        textArea.style.textDecoration = "none";
+    }
+} // function styleText
 
-
+biggerButton.onclick = increaseFontSize;
+fancyButton.onchange = styleText;
+boringButton.onchange = styleText;
 
 
