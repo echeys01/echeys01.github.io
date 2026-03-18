500e3ffa35f1fdee8c3fe8d9ded912649b1dd884
Eric Cheyssial <echeyssial@csus.edu>
2026-03-18 14:24:01 -0700

    Wrap all case changes and suffix modifications in 'modifyCaseAndSuffix.'

--- assn5-fancify/fancifymytext.js
+++ assn5-fancify/fancifymytext.js
@@ -12,21 +12,28 @@ const boringButton = document.querySelector("#boringBetty");
 const textArea = document.getElementById("userText");
 
 // Original alert for "Bigger!" click.
-/*  function alertBiggerClicked() {    
-        alert("Hello, world!");
-    } */
-
-// biggerButton.onclick = alertBiggerClicked;
+function alertBiggerClicked() {    
+    alert("Hello, world!");
+} // function alertBiggerClicked
 
 function modifyFontSize() {
-  textArea.style.fontSize = "24pt";
+    textArea.style.fontSize = "24pt";
 } // function increaseFontSize
 
-function modifyCase() {
+function modifyCaseAndSuffix() {
+    // Convert text input to all caps.
     textArea.value = textArea.value.toUpperCase();
+
+    // Append suffix "Moo" to last word of each sentence.
+    var str = textArea.value;
+    var parts = str.split(".");
+    str = parts.join("-Moo");
+
+    textArea.value = str;
 } // function modifyCase
 
 function styleText() {
+    // Assess whether "FancyShmancy" was checked, apply styling.
     if (fancyButton.checked) {
         textArea.style.fontWeight = "bold";
         textArea.style.color = "blue";
@@ -42,7 +49,7 @@ function styleText() {
 
 // Button click events.
 biggerButton.onclick = modifyFontSize;
-mooButton.onclick = modifyCase;
+mooButton.onclick = modifyCaseAndSuffix;
 
 // Radio button check events.
 fancyButton.onchange = styleText;
